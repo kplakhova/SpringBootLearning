ddaa4ccd4fecb6f986995bd7a1e55fe3fe2576ab
kplakhova <kplakhova@luxoft.com>
2018-10-05 18:17:32 +0200

    added JsonProperty

--- src/main/java/com/kplakhova/learning/model/User.java
+++ src/main/java/com/kplakhova/learning/model/User.java
@@ -1,18 +1,26 @@
 package com.kplakhova.learning.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
 import java.util.UUID;
 
 public class User {
 
     // user id
-    private  UUID userUid;
-    private String firstName;
-    private String lastName;
-    private Gender gender;
-    private Integer age;
-    private String email;
+    private final UUID userUid;
+    private final String firstName;
+    private final String lastName;
+    private final Gender gender;
+    private final Integer age;
+    private final String email;
 
-    public User(UUID userUid, String firstName, String lastName, Gender gender, Integer age, String email) {
+    public User(@JsonProperty("userUid") UUID userUid,
+                @JsonProperty("firstName") String firstName,
+                @JsonProperty("lastName") String lastName,
+                @JsonProperty("gender") Gender gender,
+                @JsonProperty("age") Integer age,
+                @JsonProperty("email") String email) {
         this.userUid = userUid;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -21,17 +29,9 @@ public class User {
         this.email = email;
     }
 
-    public User() {
-    }
-
     public UUID getUserUid() {
         return userUid;
     }
-
-    public void setUserUid(UUID userUid) {
-        this.userUid = userUid;
-    }
-
 
     public String getFirstName() {
         return firstName;
@@ -51,6 +51,11 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public static User newUser(UUID userUid, User user) {
+        return new User(userUid, user.getFirstName(), user.getLastName(), user.getGender(),
+                user.getAge(), user.getEmail());
     }
 
     @Override
