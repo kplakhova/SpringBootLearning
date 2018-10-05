bb8d98b34f233a25233891eee30bee29eb9ec348
kplakhova <kplakhova@luxoft.com>
2018-10-05 15:57:07 +0200

    refactored with ResponseEntity

--- src/main/java/com/kplakhova/learning/resource/UserResource.java
+++ src/main/java/com/kplakhova/learning/resource/UserResource.java
@@ -3,12 +3,15 @@ package com.kplakhova.learning.resource;
 import com.kplakhova.learning.model.User;
 import com.kplakhova.learning.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -35,7 +38,25 @@ public class UserResource {
             method = RequestMethod.GET,
             path = "{userUid}"
     )
-    public User fetchUser(@PathVariable("userUid") UUID userUid) {
-        return userService.getUser(userUid).get();
+    public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+        return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok).
+                orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ErrorMessage("user " + userUid + " not found.")));
+    }
+
+    class ErrorMessage {
+        String errorMessage;
+
+        public ErrorMessage(String message) {
+            this.errorMessage = message;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
     }
 }
