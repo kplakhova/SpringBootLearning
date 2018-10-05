4c40dee931c3969436254edea019316599d521ed
kplakhova <kplakhova@luxoft.com>
2018-10-05 16:37:45 +0200

    added insert new user request

--- src/main/java/com/kplakhova/learning/resource/UserResource.java
+++ src/main/java/com/kplakhova/learning/resource/UserResource.java
@@ -4,14 +4,11 @@ import com.kplakhova.learning.model.User;
 import com.kplakhova.learning.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -42,6 +39,18 @@ public class UserResource {
         return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok).
                 orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body(new ErrorMessage("user " + userUid + " not found.")));
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Integer> insertNewUser(@RequestBody User user) {
+        int result = userService.insertUser(user);
+        if (result == 1) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     class ErrorMessage {
