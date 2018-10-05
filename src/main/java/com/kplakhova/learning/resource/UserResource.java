e5f9bea8269b258f72431f36f28c0beb27801024
kplakhova <kplakhova@luxoft.com>
2018-10-05 15:24:24 +0200

    added /api/v1/users/{userUid} request mapping

--- src/main/java/com/kplakhova/learning/resource/UserResource.java
+++ src/main/java/com/kplakhova/learning/resource/UserResource.java
@@ -3,11 +3,13 @@ package com.kplakhova.learning.resource;
 import com.kplakhova.learning.model.User;
 import com.kplakhova.learning.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(
@@ -27,5 +29,13 @@ public class UserResource {
     )
     public List<User> fetchUsers() {
         return userService.getAllUsers();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "{userUid}"
+    )
+    public User fetchUser(@PathVariable("userUid") UUID userUid) {
+        return userService.getUser(userUid).get();
     }
 }
