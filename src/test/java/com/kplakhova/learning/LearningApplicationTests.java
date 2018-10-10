a46711b05e064ab30a0d746d24ef4dc9273bb9a7
kplakhova <kplakhova@luxoft.com>
2018-10-10 17:42:15 +0200

    added delete integration test

--- src/test/java/com/kplakhova/learning/LearningApplicationTests.java
+++ src/test/java/com/kplakhova/learning/LearningApplicationTests.java
@@ -8,10 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -20,17 +23,40 @@ public class LearningApplicationTests {
     @Autowired
     private UserResourceV1 userResourceV1;
 
-	@Test
-	public void itShouldFetchAllUsers() {
-        List<User> users = userResourceV1.fetchUsers(null);
-        assertThat(users).hasSize(1);
-
-        User joe = new User(null, "Joe", "Jones",
+    @Test
+    public void shouldInsertUser() {
+        //Given
+        UUID userUid = UUID.randomUUID();
+        User user = new User(userUid, "Joe", "Jones",
                 User.Gender.MALE, 22, "joe.jones@gmail.com");
 
-        assertThat(users.get(0)).isEqualToIgnoringGivenFields(joe, "userUid");
-        assertThat(users.get(0).getUserUid()).isInstanceOf(UUID.class);
-        assertThat(users.get(0).getUserUid()).isNotNull();
+        //When
+        userResourceV1.insertNewUser(user);
+
+        //Then
+        User joe = userResourceV1.fetchUser(userUid);
+        assertThat(joe).isEqualToComparingFieldByField(user);
     }
 
+    @Test
+    public void shouldDeleteUser() {
+        //Given
+        UUID userUid = UUID.randomUUID();
+        User user = new User(userUid, "Joe", "Jones",
+                User.Gender.MALE, 22, "joe.jones@gmail.com");
+
+        //When
+        userResourceV1.insertNewUser(user);
+
+        //Then
+        User joe = userResourceV1.fetchUser(userUid);
+        assertThat(joe).isEqualToComparingFieldByField(user);
+
+        //When
+        userResourceV1.deleteUser(userUid);
+
+        //Then
+        assertThatThrownBy(() -> userResourceV1.fetchUser(userUid))
+                .isInstanceOf(NotFoundException.class);
+    }
 }
